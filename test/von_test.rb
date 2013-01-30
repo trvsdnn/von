0eb0bb82f964a094e760c218c672828d23d65dd0
blahed <trvsdnn@gmail.com>
2013-01-29 19:11:02 -0500

    fix connection error test

--- test/von_test.rb
+++ test/von_test.rb
@@ -7,8 +7,7 @@ describe Von do
   end
 
   it "increments a counter and counts it" do
-    @redis = Redis.new
-    @redis.flushall
+    Redis.new.flushall
     
     Von.increment('foo')
     Von.increment('foo')
@@ -17,7 +16,7 @@ describe Von do
 
   it "raises a Redis connection errors if raise_connection_errors is true" do
     Von.config.raise_connection_errors = true
-    Redis.expects(:new).raises(Redis::CannotConnectError)
+    Von.expects(:increment).raises(Redis::CannotConnectError)
 
     lambda { Von.increment('foo') }.must_raise Redis::CannotConnectError
   end
